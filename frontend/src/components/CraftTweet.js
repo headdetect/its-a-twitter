1a8b1f4f28c473b2d83b77b34e1de47e6098f206
headdetect <headdetective2@gmail.com>
2022-04-24 23:26:50 -0400

    Clean up around file uploads

--- frontend/src/components/CraftTweet.js
+++ frontend/src/components/CraftTweet.js
@@ -3,17 +3,27 @@ import React from "react";
 import "./Tweet.css";
 import "./CraftTweet.css";
 
+const ACCEPTABLE_MIME_TYPES = ["image/jpeg", "image/gif", "image/png"];
+
 export default function CraftTweet({ onTweet = async (_, __) => {} }) {
   const [text, setText] = React.useState("");
   const [file, setFile] = React.useState(null);
+  const [error, setError] = React.useState(null);
 
   const fileInputRef = React.useRef(null);
 
   const handleSubmitTweet = async () => {
+    if (!ACCEPTABLE_MIME_TYPES.includes(file.type)) {
+      setError(
+        "This file type is not allowed. You must choose an image type (gif, png, jpeg)",
+      );
+    }
+
     await onTweet(text.trim(), file);
 
     setText("");
     setFile(null);
+    setError(null);
 
     if (fileInputRef.current) {
       fileInputRef.current.value = "";
@@ -37,6 +47,8 @@ export default function CraftTweet({ onTweet = async (_, __) => {} }) {
             <input
               type="file"
               onChange={e => setFile(e.target.files?.[0])}
+              multiple={false}
+              accept={ACCEPTABLE_MIME_TYPES.join(", ")}
               ref={fileInputRef}
             />
           </div>
@@ -47,6 +59,17 @@ export default function CraftTweet({ onTweet = async (_, __) => {} }) {
           >
             Post
           </button>
+          {error && (
+            <div
+              style={{
+                background: "#f49697",
+                border: "1px solid #e92e32",
+                padding: 8,
+              }}
+            >
+              {error}
+            </div>
+          )}
         </div>
       </div>
     </div>
